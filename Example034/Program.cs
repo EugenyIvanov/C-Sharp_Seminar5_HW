c81a9a35a73db49f6d4b7d3d62861ecbf521fdb5
Евгений <ivanychcos@gmail.com>
2022-09-11 10:36:44 +0300

    Add example 034

---
+++ Example034/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] array = GetArray(size, 100, 999);
+
+int [] GetArray(int size, int min, int max)
+{
+   int [] result = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    result[i] = new Random().Next(min, max + 1);
+   }
+   return result;
+}
+Console.WriteLine(String.Join(" ", array));
+int count = 0;
+for(int i = 0; i <array.Length; i++)
+{
+    if(array[i] % 2 == 0) count++;
+}
+Console.WriteLine(count);
